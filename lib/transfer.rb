f0bb083685f33259a48ef9160a04f19ddb10e501
Hannah Malm <hannahemalm616@gmail.com>
2021-02-24 01:29:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   def execute_transaction
   
     if @sender.valid? == false 
-      return "Transaction rejected. Please check your account balance."
+       "Transaction rejected. Please check your account balance."
     else
       # #execute transaction by sending an amount from the senders bank account to the receiver bank account 
       # #update the balance by taking balance - amount 
